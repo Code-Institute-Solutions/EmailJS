a1013062fe043362c6ee602f07bdd7870ed28b56
Yoni Lavi <yoni@codeinstitute.net>
2018-03-09 11:42:53 +0000

    a possible way to add success/error messages

--- 03-sending_emails/assets/js/sendEmail.js
+++ 03-sending_emails/assets/js/sendEmail.js
@@ -1,16 +1,33 @@
 function sendMail(contactForm) {
+    var contactName = contactForm.name.value;
+    var contactEmail = contactForm.emailaddress.value;
+    var contactRequest = contactForm.projectsummary.value;
+    var formMessageElem = document.getElementById('form-message');
+
     emailjs.send("gmail", "rosie", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
+            "from_name": contactName,
+            "from_email": contactEmail,
+            "project_request": contactRequest
+        })
+        .then(
+            function(response) {
+                // Show success alert to user
+                formMessageElem.innerHTML =
+                    `Thank you ${contactName}, I'll be in touch soon, have a great day`;
+                formMessageElem.classList.add("alert-success");
+
+                contactForm.reset();
+            },
+            function(error) {
+                // Show error alert to user
+                formMessageElem.innerHTML =
+                    `Sorry, we seem to have a technical difficulty, please try again later.`;
+                formMessageElem.classList.add("alert-danger");
+
+                // Show full error object in console
+                console.error("Error received from emailJS:", JSON.parse(error.text));
+            }
+        );
+
     return false;  // To block from loading a new page
 }
